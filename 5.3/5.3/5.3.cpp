3386b40e5b563ce397af8d5bf60b5ac12e4eb497
Sadetrin <albertsienko@gmail.com>
2024-11-08 10:49:35 +0300

    Add files via upload

---
+++ 5.3/5.3/5.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x, f;
+	cout << "Введите значение x: " << endl;
+	cin >> x;
+	if (x <= 3)
+	{
+		f = 2 * x + 3;
+		cout << "Значение функции f(x): " << f << endl;
+	}
+	else if (x > -3 && x < 0.5)
+	{
+		f = -7;
+		cout << "Значение функции f(x): " << f << endl;
+	}
+	else if(x>=5)
+	{
+		f = (x*x)/5;
+		cout << "Значение функции f(x): " << f << endl;
+	}
+
+
+	return 0;
+}
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double a, b, c, D, x1, x2;
+    do {
+        cout << "Введи значение a, b, c : " << endl;
+        cin >> a >> b >> c;
+        if (a == 0)
+        {
+            cout << "Введено не приемлимое значение, повторите попытку " << endl;
+        }
+    } while (a == 0);
+    D = b * b - 4 * a * c;
+    if (D == 0)
+    {
+        x1 = -b / (2 * a);
+        cout << "Уравнение имеет один корень: " << x1 << endl;
+
+    }
+    else if (D < 0) {
+        cout << "Действительных корней нет" << endl;
+
+    }
+    else {
+        x1 = (-b - sqrt(D)) / (2 * a);
+        x2 = (-b + sqrt(D)) / (2 * a);
+        cout << "Уравнение имеет два корня: " << endl << x1 << endl << x2 << endl;
+    };
