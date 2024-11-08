f42be0067bd75c018c708a0229e94156bffbb062
Sadetrin <albertsienko@gmail.com>
2024-11-08 10:52:35 +0300

    Add files via upload

---
+++ Лаба 8/Лаба 8/Лаба 8.cpp
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+double graph(double x)
+{
+	const double R = 1.0;
+	if (x >= -3 && x <= -2) {
+		// Линейный участок с наклоном 45° (y = x + 3)
+		return x + 3;
+	}
+	else if (x > -2 && x <= -1) {
+		// Участок дуги окружности (центр окружности (-2, 1), радиус 1)
+		double value = R * R - pow(x + 2, 2);
+		if (value < 0) return 0;
+		return sqrt(value) + R;
+	}
+	else if (x > -1 && x <= 1) {
+		// Прямая линия, параллельная оси X (y = 1)
+		return 1;
+	}
+	else if (x > 1 && x <= 2) {
+		// Линейный участок от (1, 1) до (2, -1)
+		return -2 * (x - 1) + 1;
+	}
+	else if (x > 2) {
+		// Прямая линия, параллельная оси X (y = -1)
+		return -1;
+	}
+	return 0;
+}
+int function(int num)
+{
+	if (num % 7 == 0) {
+		return num / 7;
+	}
+	else {
+		return num + 7;
+	}
+}
+void delitel(int n, int divisor = 2)
+{
+	if (n <= 1) return;
+	if (n % divisor == 0)
+	{
+		cout << divisor << ", ";
+		delitel(n / divisor, divisor);
+	}
+	else {
+		delitel(n, divisor + 1);
+	}
+}
+void task1()
+{
+	double x_start = -3.0, x_end = 5.0, dx = 0.1;
+	double y, R = 1;
+
+	cout << "x\t\ty" << endl;
+	cout << "------------------" << endl;
+	for (double x = x_start; x <= x_end; x += dx)
+	{
+		double y = graph(x);
+		cout << x << "\t" << y << endl;
+
+	}
+}
+void task2()
+{
+	int number;
+	cout << "Введите число: " << endl; cin >> number;
+	int result = function(number);
+	cout << "Измененное число: " << result << endl;
+
+}
+void task3()
+{
+	int n;
+	cout << "Введите натуральное число n>1: "; cin >> n;
+	cout << "Простые делители числа " << n << ": ";
+	delitel(n);
+	cout << endl;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int task;
+	int number;
+	while (true)
+	{
+		cout << "Введите номер задания (0 для выхода): " << endl;
+		cin >> task;
+		if (task == 0)
+		{
+			break;
+		}
+		switch (task)
+		{
+		case 1:
+		{
+			task1();
+			break;
+		}
+		case 2:
+		{
+			task2();
+			break;
+		}
+		case 3:
+		{
+			task3();
+			break;
+		}
+		default:
+			cout << "Введен неверный номер задания " << endl;
+		}
+	}
+	cout << "Программа завершена." << endl;
+	return 0;
+}
---
+++ лаба 9/лаба 9/лаба 9.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+using namespace std;
+void task1()
+{
+	long int value2, value1=200000;
+	long* yk;
+	yk = &value1;
+	cout << *yk << endl;
+	value2 = *yk;
+	cout << value2 << endl;
+	cout << &value1 << endl;
+	cout << yk << endl;
+}
+void swap(int& x, int& y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+
+}
+void task2()
+{
+	int a, b;
+	cout << "Введите значение a и b: " << endl; cin >> a >> b;
+	cout << "a=" << a << " b=" << b << endl;
+	swap(a, b);
+	cout << "a=" << a << " b=" << b << endl;
+}
+void vychitanie(int& n)
+{
+	int count = 0;
+	while (n != 0)
+	{
+		int temp = n;
+		int sum = 0;
+		while (temp != 0)
+		{
+			sum += temp % 10;
+			temp /= 10;
+		}
+		n -= sum;
+		count++;
+	}
+	cout << "Количесвто вычитаний = " << count << endl;
+}
+void task3()
+{
+	int chislo;
+	cout << "Введите число: " << endl; cin >> chislo;
+	vychitanie(chislo);
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int task;
+	while (true)
+	{
+		cout << "Введите номер задания (0 для выхода) " << endl; cin >> task;
+		if (task == 0)
+		{
+			break;
+		}
+		switch (task)
+		{
+		case 1:
+		{
+			task1();
+			break;
+		}
+		case 2:
+		{
+			task2();
+			break;
+		}
+		case 3 :
+		{
+			task3();
+			break;
+		}
+		default:
+			cout << "Неверный номер задания! " << endl;
+		}
+
+
+	}
+	cout << "Программа завершена. " << endl;
+	return 0;
+}
+/*
+В задании 2 из представленных варинатов не правильно решает задачу первый пример, так как функция принимает параметры по значению, 
+что означает создание локальных копий x и y внутри функции swap. Изменения произведенные в этих локальных копиях не отразяться на переменных
+a и b
+2 и 3 примеры правильно решают задачу, но боллее предпочтительный вариант 3, т.к. он более читаем и не требует работы с 
+указателями, что делает код проще
+ */
+//в задании 3 программа показывает, как работают указатели и операции взятия адреса и разыменования в C++.
